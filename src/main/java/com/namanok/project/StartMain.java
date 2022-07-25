63f468a5afe27c1783ec8f19c442e51b81d7d9ae
test <oo2706@naver.com>
2022-07-25 23:22:30 +0900

    파일 수정

---
+++ src/main/java/com/namanok/project/StartMain.java
@@ -0,0 +1,10 @@
+package com.namanok.project;
+
+public class StartMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("START!!");
+	}
+
+}
