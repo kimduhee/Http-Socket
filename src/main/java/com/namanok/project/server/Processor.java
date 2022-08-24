2d944bbe61e1220b52d835b73e1b808b2def2e22
test <oo2706@naver.com>
2022-08-24 21:52:03 +0900

    파일수정

--- src/main/java/com/namanok/project/server/Processor.java
+++ src/main/java/com/namanok/project/server/Processor.java
@@ -1,9 +1,22 @@
 package com.namanok.project.server;
 
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+
 public class Processor implements Runnable {
 	
 	@Override
     public void run() {
-		System.out.println("실행!!!!");
+		System.out.println("thread 실행!!!!");
+		
+		OutputStream raw = null;
+        DataOutputStream dos = null;
+        
+        try {
+        	
+        } catch(Exception e) {
+        	
+        }
 	}
 }
+ 
